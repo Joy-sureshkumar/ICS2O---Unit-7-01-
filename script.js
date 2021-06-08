b985a0f543db1c2f15157d199feae509345e4a72
Joy Sureshkumar <>
2021-06-08 13:59:49 +0000

    new

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ let randomNumber = 0
 let userGuess = 0
 
 function randomizer () {
-  userGuess = document.getElementById('input').value
-  userGuess = parseInt(userGuess)
+ userGuess = document.getElementById('input').value
+ userGuess = parseInt(userGuess)
 
-  randomNumber = (Math.random() * 6) + 1
+ randomNumber = (Math.random() * 6) + 1
 
-  randomNumber = parseInt(randomNumber)
+ randomNumber = parseInt(randomNumber)
 
-  if (userGuess === randomNumber) {
-    document.getElementById('answer').innerHTML = 'Winner!'
-
+ if (userGuess === randomNumber) {
+ document.getElementById('answer').innerHTML = 'Winner!'
   }
 }
